0eb8e371a2f774021d7e182c53278321c9a94ecd
Zigordion <sigurdion@live.dk>
2024-03-19 18:22:57 +0100

    fix css

--- frontend/src/components/WeatherComponent.tsx
+++ frontend/src/components/WeatherComponent.tsx
@@ -9,8 +9,9 @@ interface WeatherComponentProps{
 const WeatherComponent = ({value, valueName, imagePath, altText} : WeatherComponentProps ) => {
     return (
         <div className='weather-component'>
-            <h3>{valueName} {value!=null ? value : '0.0'}</h3>
             <img height={50} className='weather-icon' src={imagePath} alt={altText}></img>
+            <h3>{valueName} </h3>
+            <h3>{value!=null ? value : '0.0'}</h3>
         </div>
     )
 }
