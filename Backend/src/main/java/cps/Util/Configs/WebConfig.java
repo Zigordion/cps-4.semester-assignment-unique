756f8f3b3ca71ad6b180f14104ccbda32ec9f73f
Zigordion <sigurdion@live.dk>
2024-05-24 13:15:45 +0200

    refactor CORS to only allow from port 3000 localhost

--- Backend/src/main/java/cps/Util/Configs/WebConfig.java
+++ Backend/src/main/java/cps/Util/Configs/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/api/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET")
                 .maxAge(3600);
     }
